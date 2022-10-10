60625ede40e7c8c2bab7f3cdc9f7c7ebf6d2ce0a
LainWiredIn <manav.chaudhary@research.iiit.ac.in>
2022-10-10 23:39:39 +0530

    Fixed Protein Viewer

--- frontend.py
+++ frontend.py
@@ -9,21 +9,23 @@ generate = ml.form_submit_button("Generate Protein PDB")
 if generate:
     print("Hello")
 
-def render_mol(xyz):
-    xyzview = py3Dmol.view(width=400,height=400)
-    xyzview.addModel(xyz,'xyz')
-    xyzview.setStyle({'color':'spectrum'})
-    xyzview.setBackgroundColor('white')
-    xyzview.zoomTo()
+def render_mol(pdb):
+    pdbview = py3Dmol.view(width=400,height=400)
+    pdbview.addModelsAsFrames(pdb)
+    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
+    # pdbview.setBackgroundColor('white')
+    pdbview.zoomTo()
     if spin:
-        xyzview.spin(True)
+        pdbview.spin(True)
     else:
-        xyzview.spin(False)
-    showmol(xyzview, height = 500,width=800)
+        pdbview.spin(False)
+    showmol(pdbview, height = 500,width=800)
 
-model = st.sidebar.container
 spin = st.sidebar.checkbox('Spin', value = False)
 uploaded_file = st.sidebar.file_uploader("Choose the file to model")
 if uploaded_file is not None:
-    xyz = uploaded_file.getvalue().decode("utf-8")
-    render_mol(xyz)
+    # pdb = uploaded_file.getvalue().decode("utf-8")
+    with open("cocaine.pdb") as ifile:
+        pdb = "".join([x for x in ifile])
+
+    render_mol(pdb)
