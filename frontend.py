7f917db4e327ef217623f4a5e5c01804bc027876
Mugundan Kottur Suresh <mugundan.kottur@gmail.com>
2022-10-10 22:15:51 +0530

    Improved Front End

--- frontend.py
+++ frontend.py
@@ -2,8 +2,12 @@ import streamlit as st
 import py3Dmol
 from stmol import showmol
 st.sidebar.title('Openfold')
-protein = st.sidebar.text_input('Enter a protein name', '1A2C')
-spin = st.sidebar.checkbox('Spin', value = False)
+ml = st.sidebar.form("Protein Model Generation")
+protein = ml.text_input('Enter a protein sequence')
+generate = ml.form_submit_button("Generate Protein PDB")
+
+if generate:
+    print("Hello")
 
 def render_mol(xyz):
     xyzview = py3Dmol.view(width=400,height=400)
@@ -17,7 +21,9 @@ def render_mol(xyz):
         xyzview.spin(False)
     showmol(xyzview, height = 500,width=800)
 
-uploaded_files = st.sidebar.file_uploader("Choose xyz files")
-for uploaded_file in uploaded_files:
+model = st.sidebar.container
+spin = st.sidebar.checkbox('Spin', value = False)
+uploaded_file = st.sidebar.file_uploader("Choose the file to model")
+if uploaded_file is not None:
     xyz = uploaded_file.getvalue().decode("utf-8")
     render_mol(xyz)
